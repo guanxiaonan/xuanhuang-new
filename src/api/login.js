06d38a1d5aa3c52a52ef341d7123477d3beaca60
apple <420201636@qq.com>
2018-12-10 19:22:53 +0800

    first push 12-10

---
+++ src/api/login.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function loginByUsername(data) {
+  // const data = JSON.stringify(query)
+  return request({
+    url: 'http://101.132.44.253:8767/background/login',
+    method: 'post',
+    data
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/login/logout',
+    method: 'post'
+  })
+}
+
+export function getUserInfo(token) {
+  return request({
+    url: '/server/index.php?g=Web&c=Mock&o=simple&projectID=15&uri=getUserInfo',
+    method: 'post',
+    params: {
+      token
+    }
+  })
+}
